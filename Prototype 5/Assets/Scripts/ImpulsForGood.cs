43bfb4baae8b9d43a9f6c9cb7467c9bf307e5be9
ivan-zhukouski <stigmatix.by@gmail.com>
2020-12-09 17:34:33 +0300

    added prototype_5

---
+++ Prototype 5/Assets/Scripts/ImpulsForGood.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpulsForGood : MonoBehaviour
+{
+    private Rigidbody prefabRB;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prefabRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Bad"))
+        {
+            prefabRB.AddForce(Vector3.up * speed, ForceMode.Impulse );
+        }
+    }
+}
---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    private float minSpeed = 12f;
+    private float maxSpeed = 17f;
+    private float torque = 10f;
+    private float xRange= 4f;
+    private float ySpawnPos = -1.5f;
+
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomPos();
+    }
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed,maxSpeed);
+    }
+    float RandomTorque()
+    {
+        return Random.Range(-torque,torque);
+    }
+    Vector3 RandomPos()
+    {
+        return new Vector3(Random.Range(-xRange,xRange), ySpawnPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        if(gameManager.isGameActive)
+        {
+             Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+       
+    }
+    private void OnTriggerEnter(Collider other) {
+        Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+        
+    }
+
+}
